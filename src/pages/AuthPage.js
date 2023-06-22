c8ad1d033918d968d042a05bd329b370e6641f2d
Hnatiak <58102343+Hnatiak@users.noreply.github.com>
2023-06-22 21:23:08 +0200

    Проблеми вирішено

--- src/pages/AuthPage.js
+++ src/pages/AuthPage.js
@@ -1,19 +1,13 @@
 import { useParams } from 'react-router-dom';
-import { Suspense } from 'react';
-import { Outlet } from 'react-router-dom';
 import RegisterForm from '../components/RegisterForm/RegisterForm'
 import LoginForm from '../components/LoginForm/LoginForm'
 
 const AuthPage = () => {
   const { id } = useParams();
-  console.log(id)
   return (
     <div>
       {id === 'login' && <LoginForm />}
       {id === 'register' && <RegisterForm /> }
-      <Suspense>
-      <Outlet />
-    </Suspense>
     </div>
   );
 };
