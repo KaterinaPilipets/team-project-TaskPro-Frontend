dcc25f7f448015362f18251fc78e1912aff2d14d
KaterinaPilipets <ket.pil.glass@gmail.com>
2023-06-23 07:28:20 +0300

    fix auth

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
 // import { Link } from 'react-router-dom';
 import { Link } from '@mui/material';
 import css from './LoginForm.module.css';
@@ -7,26 +7,29 @@ import css from './LoginForm.module.css';
 // import eyeOpen from '../../images/eye-open.svg'
 
 function LoginPage() {
-  const [showPassword, setShowPassword] = useState(false);
+  // const [showPassword, setShowPassword] = useState(false);
 
-  const togglePasswordVisibility = () => {
-    setShowPassword(!showPassword);
-  };
-
+  // const togglePasswordVisibility = () => {
+  //   setShowPassword(!showPassword);
+  // };
 
   return (
     <div className={css.content}>
       <div className={css.menu}>
-        <Link href="register" underline="none">Registration</Link>
-        <Link href="login" underline="none">Login</Link>
+        <Link href="register" underline="none">
+          Registration
+        </Link>
+        <Link href="login" underline="none">
+          Login
+        </Link>
       </div>
       <div className={css.inputs}>
-        <input
-            type="email"
-            placeholder="Email"
-        />
+        <input type="email" placeholder="Email" />
         <div className={css.passwordinput}>
-          <input type={showPassword ? 'text' : 'password'} placeholder="Password" />
+          <input
+            // type={showPassword ? 'text' : 'password'}
+            placeholder="Password"
+          />
           {/* <span className={`${css.togglepassword} ${css.toggleIcon}`} onClick={togglePasswordVisibility}>
             {showPassword ? (
               <FontAwesomeIcon icon={faEyeSlash} width='18px' color='#737373'/>
--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
 // import { Link } from 'react-router-dom';
 import { Link } from '@mui/material';
 import css from './RegisterForm.module.css';
@@ -7,26 +7,31 @@ import css from './RegisterForm.module.css';
 // import eyeOpen from '../../images/eye-open.svg'
 
 function RegistrationPage() {
-  
+  // const [showPassword, setShowPassword] = useState(false);
 
-  const [showPassword, setShowPassword] = useState(false);
-
-  const togglePasswordVisibility = () => {
-    setShowPassword(!showPassword);
-  }
+  // const togglePasswordVisibility = () => {
+  //   setShowPassword(!showPassword);
+  // }
 
   return (
     <div className={css.content}>
       <div className={css.menu}>
-        <Link className={css.style} href="register" underline="none">Registration</Link>
-        <Link className={css.style} href="login" underline="none">Login</Link>
+        <Link className={css.style} href="register" underline="none">
+          Registration
+        </Link>
+        <Link className={css.style} href="login" underline="none">
+          Login
+        </Link>
       </div>
       <div className={css.inputs}>
         <input type="text" placeholder="Name" />
         <input type="email" placeholder="Email" />
         <div className={css.passwordinput}>
-        <input type={showPassword ? 'text' : 'password'} placeholder="Password" />
-        {/* <span className={`${css.togglepassword} ${css.toggleIcon}`} onClick={togglePasswordVisibility} style={{ cursor: 'pointer' }}>
+          <input
+            // type={showPassword ? 'text' : 'password'}
+            placeholder="Password"
+          />
+          {/* <span className={`${css.togglepassword} ${css.toggleIcon}`} onClick={togglePasswordVisibility} style={{ cursor: 'pointer' }}>
             {showPassword ? (
               <FontAwesomeIcon icon={faEyeSlash} width='18px' color='#737373'/>
               ) : (
@@ -35,7 +40,7 @@ function RegistrationPage() {
           </span> */}
         </div>
       </div>
-      <button className={css.registerbtn} >Register Now</button>
+      <button className={css.registerbtn}>Register Now</button>
     </div>
   );
 }
