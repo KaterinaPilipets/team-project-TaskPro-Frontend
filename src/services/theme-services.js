7a76ca602273e4a5bea0fe7b889a6d4c1d66c973
Kate-Lesnyak <catharinalesnyak@gmail.com>
2023-06-24 19:33:08 +0300

    theme, chang css, component theme

---
+++ src/services/theme-services.js
@@ -0,0 +1,9 @@
+// import axios from "axios";
+
+// const themeInstance = axios.create({
+//   baseURL: "https://6"
+// })
+
+// export const updateTheme = (data) => {
+//   return themeInstance.patch("/theme", data)
+// };
