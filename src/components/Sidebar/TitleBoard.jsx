9791740462ef4a9c689c1100577729c3bc63cdb8
LudaFesenko <108350058+LudaFesenko@users.noreply.github.com>
2023-06-25 15:46:32 +0300

    add styles

--- src/components/Sidebar/TitleBoard.jsx
+++ src/components/Sidebar/TitleBoard.jsx
@@ -4,7 +4,16 @@ import Typography from '@mui/material/Typography';
 const TitleBoard = () => {
   return (
     <div>
-      <Typography color="textSecondary" sx={{ px: 1.75, py: 1 }}>
+      <Typography
+        color="textSecondary"
+        sx={{
+          fontSize: 12,
+          lineHeight: 1.5,
+          letterSpacing: '-0.02em',
+          py: 1,
+          color: 'var( --inputText)',
+        }}
+      >
         My boards
       </Typography>
     </div>
