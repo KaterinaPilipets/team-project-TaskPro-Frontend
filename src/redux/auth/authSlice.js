ecd69c65a485a1838a639c5e22efa8dc82f5dd2a
Hnatiak <58102343+Hnatiak@users.noreply.github.com>
2023-06-26 23:02:01 +0200

    Пофіксили помилки

--- src/redux/auth/authSlice.js
+++ src/redux/auth/authSlice.js
@@ -1,6 +1,30 @@
+// import { createSlice } from '@reduxjs/toolkit';
+
+// const authSlice = createSlice({
+//   name: 'auth',
+//   initialState: {
+//     user: null,
+//     // token: null,
+//   },
+//   reducers: {
+//     setUser(state, action) {
+//       state.user = action.payload;
+//     },
+//     setToken(state, action) {
+//       state.user.token = action.payload;
+//     },
+//   },
+// });
+
+// export const { setUser, setToken } = authSlice.actions;
+
+// export default authSlice.reducer;
+
+
+
 import { createSlice } from '@reduxjs/toolkit';
 
-const authSlice = createSlice({
+export const authSlice = createSlice({
   name: 'auth',
   initialState: {
     user: null,
