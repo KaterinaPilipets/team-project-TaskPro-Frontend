51c70a07231615e8097b5fcddd72ffd3676f64d8
Hnatiak <58102343+Hnatiak@users.noreply.github.com>
2023-06-27 15:39:40 +0200

    Доробив LogOut

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -38,3 +38,54 @@ const dispatch = useDispatch();
     </>
   );
 };
+
+
+
+// // import { Suspense, lazy, useEffect } from 'react';
+// import { Suspense, lazy, useEffect } from 'react';
+// import { Routes, Route } from 'react-router-dom';
+// import { SharedLayout } from './SharedLayout/SharedLayout.js';
+// import { useDispatch } from 'react-redux';
+// import { setToken } from 'redux/auth/authSlice.js';
+// import PrivateRoute from './PrivateRouter.js';
+// // import { useDispatch, useSelector } from 'react-redux';
+
+// const WelcomePage = lazy(() => import('../pages/WelcomePage'));
+// const AuthPage = lazy(() => import('../pages/AuthPage'));
+// const HomePage = lazy(() => import('../pages/HomePage'));
+// export const App = () => {
+// const dispatch = useDispatch();
+// // const isRefreshing = useSelector(authSelector.getIsRefreshing);
+
+//    useEffect(() => {
+//     // const token = localStorage.getItem('token');
+//     dispatch(setToken);
+//     //  dispatch(authOperation.refreshCurrentUser());
+//    }, [dispatch]);
+
+//   return (
+//     <>
+//       <Suspense fallback={<p>Loading...</p>}>
+//         {/* <Container> */}
+//         <Routes>
+//           <Route path="/" element={<WelcomePage />} />
+//           <Route path="/welcome" element={<WelcomePage />} />
+//           <Route
+//           path="/"
+//           element={
+//               <PrivateRoute redirectTo="/auth/:id" component={<SharedLayout />}>
+//                 {/* <Route index element={<HomePage />} /> */}
+//                 <Route path="/home" element={<SharedLayout />}>
+//                   <Route index element={<HomePage />} />
+//                 </Route>
+//               </PrivateRoute>
+//             }
+//           />
+//         <Route path="/auth/:id" element={<AuthPage />} />
+//           {/* <Route path="*" element={<WelcomePage />} /> */}
+//         </Routes>
+//         {/* </Container> */}
+//       </Suspense>
+//     </>
+//   );
+// };
