1ca04bea01233d0a7e7e4d4243bdea6519cf46d4
YaroslavYu <yaroslavyu801@gmail.com>
2023-06-29 22:03:44 +0300

    connect scrollbar styles in index

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
+import './GlobalScrollbarStyles.css';
 import { Provider } from 'react-redux';
 import { store } from 'redux/store';
 // persistor
