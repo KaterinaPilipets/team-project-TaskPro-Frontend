1736f31c42d43f0c68dbee24eaeaaeb5606bf804
Hnatiak <58102343+Hnatiak@users.noreply.github.com>
2023-06-29 13:06:58 +0200

    Виправив роутери і їх перенаправлення

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -44,13 +44,12 @@
 //   );
 // };
 
-
 import { Suspense, lazy, useEffect } from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import { SharedLayout } from './SharedLayout/SharedLayout.js';
 import { useDispatch, useSelector } from 'react-redux';
 import { ScreenPage } from 'pages/ScreenPage.js';
-import { PrivateRouter, PublicRouter } from './RoutesAuthOperation.js';
+import { PrivateRouter } from './RoutesAuthOperation.js'; // PublicRouter
 import { refreshUser } from 'redux/auth/authOperations.js';
 import { Loader } from './Loader/Loader.jsx';
 import { isRefreshing, isLoggedIn } from 'redux/auth/authSelectors.js';
@@ -74,21 +73,27 @@ export const App = () => {
     <>
       <Suspense fallback={<p>Loading...</p>}>
         <Routes>
-          {loggedUser ? (
-            <>
-              <Route path="/" element={<Navigate to="/home" />} />
-              <Route path="/home" element={<PrivateRouter component={<SharedLayout />} />} >
-                <Route index element={<HomePage />} />
-                <Route path=":boardId" element={<ScreenPage />} />
-              </Route>
-            </>
-          ) : (
-            <>
-              <Route path="/" element={<PublicRouter component={<WelcomePage />} />} />
-              <Route path="/welcome" element={<WelcomePage />} />
-              <Route path="/auth/:id" element={<AuthPage />} />
-            </>
-          )}
+          <Route path="/" element={
+              loggedUser ? (
+                <Navigate to="/home" replace={true} />
+              ) : (
+                <WelcomePage />
+              )
+            }
+          />
+          <Route path="/welcome" element={<WelcomePage />} />
+          <Route path="/auth/:id" element={<AuthPage />} />
+          <Route path="/home" element={
+              loggedUser ? (
+                <PrivateRouter component={<SharedLayout />} />
+              ) : (
+                <Navigate to="/" replace={true} />
+              )
+            }
+          >
+            <Route index element={<HomePage />} />
+            <Route path=":boardId" element={<ScreenPage />} />
+          </Route>
         </Routes>
       </Suspense>
     </>
