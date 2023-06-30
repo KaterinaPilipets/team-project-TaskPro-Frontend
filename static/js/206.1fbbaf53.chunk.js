68723f2ad2fa30556a182003a9221fa29bca87a9
KaterinaPilipets <109768011+KaterinaPilipets@users.noreply.github.com>
2023-06-30 23:29:22 +0000

    Deploying to gh-pages from @ KaterinaPilipets/team-project-TaskPro-Frontend@36829a1ef7e92f7c9d06809c11c25ff0ebdbf1bc 🚀

---
+++ static/js/206.1fbbaf53.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[206],{1206:function(e,t,r){r.r(t);var a=r(2839),c=r(184);t.default=function(){return(0,c.jsx)("div",{children:(0,c.jsx)(a.$,{})})}}}]);
+//# sourceMappingURL=206.1fbbaf53.chunk.js.map
