61a7412ee4eedc51ee1e9a10432e5697dd146c51
KaterinaPilipets <109768011+KaterinaPilipets@users.noreply.github.com>
2023-06-30 19:43:10 +0000

    Deploying to gh-pages from @ KaterinaPilipets/team-project-TaskPro-Frontend@77c62dded26b36342c19aee7587dde532ecd3689 🚀

---
+++ static/js/206.cd29ceda.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[206],{1206:function(e,t,r){r.r(t);var a=r(5740),c=r(184);t.default=function(){return(0,c.jsx)("div",{children:(0,c.jsx)(a.$,{})})}}}]);
+//# sourceMappingURL=206.cd29ceda.chunk.js.map
