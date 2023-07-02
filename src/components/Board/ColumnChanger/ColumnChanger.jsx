072a7debc305a531d9aed3ec90f1b6739a970d27
serhiikravchak <105275364+serhiikravchak@users.noreply.github.com>
2023-07-02 23:41:58 +0200

    fixing

--- src/components/Board/ColumnChanger/ColumnChanger.jsx
+++ src/components/Board/ColumnChanger/ColumnChanger.jsx
@@ -1,34 +1,44 @@
-import { useSelector } from 'react-redux';
-import { columns } from '../../../redux/board/boardSelector';
+import { useDispatch, useSelector } from 'react-redux';
+import { columns, cards } from '../../../redux/board/boardSelector';
 import {
-    Wrapper,
+  Wrapper,
   ColumnList,
   ColumnItem,
   ColumnTitle,
   ColumnIcon,
   ColumnChangerButton,
-  
 } from './ColumnChanger.styled';
 import icon from '../../../sourse/sprite.svg';
+import { updateCard } from 'services/board-servises';
 
-const ColumnChanger = ({onClose}) => {
+const ColumnChanger = ({ onClose, id }) => {
   const getColumns = useSelector(columns);
-  console.log(getColumns);
+  const getCards = useSelector(cards);
+  const columnId = getCards.find(cards => cards._id.includes(id)).cardOwner;
+  const dispatch = useDispatch();
 
+  const moveCard = columnId => {
+    const value = { cardOwner: columnId };
+
+
+    dispatch(updateCard({id}, value));
+  };
   return (
     <>
       <Wrapper>
         <ColumnList>
-          {getColumns.map(({ _id, title }) => (
-            <ColumnItem key={_id} onClick={() => {}}>
-              <ColumnChangerButton>
-                <ColumnTitle>{title}</ColumnTitle>
-                <ColumnIcon width="16" height="16">
-                  <use xlinkHref={`${icon}#${'icon-move'}`} />
-                </ColumnIcon>
-              </ColumnChangerButton>
-            </ColumnItem>
-          ))}
+          {getColumns
+            .filter(column => column._id !== columnId)
+            .map(({ _id, title }) => (
+              <ColumnItem key={_id}>
+                <ColumnChangerButton onClick={() => moveCard(_id)}>
+                  <ColumnTitle>{title}</ColumnTitle>
+                  <ColumnIcon width="16" height="16">
+                    <use xlinkHref={`${icon}#${'icon-move'}`} />
+                  </ColumnIcon>
+                </ColumnChangerButton>
+              </ColumnItem>
+            ))}
         </ColumnList>
       </Wrapper>
     </>
