972c3338398eccf37a498db456268f7b606dcab9
serhiikravchak <105275364+serhiikravchak@users.noreply.github.com>
2023-07-02 14:54:25 +0200

    card styles fixed, deadline bell added

--- src/components/EditPanelCard/EditPanelCard.js
+++ src/components/EditPanelCard/EditPanelCard.js
@@ -1,13 +1,19 @@
 import { Modal } from 'components/Modal';
 // import { useState } from 'react';
 import icon from '../../sourse/sprite.svg';
-import { Wrap } from './EditPanelCard.Styled';
+import {
+  Wrap,
+  DeadlineIcon,
+  DeadlineWrapper,
+  DeadlineIconBlur,
+} from './EditPanelCard.Styled';
 import { SvgBtn } from 'components/SvgBtn/SvgBtn';
 import { useToggleModal } from 'hooks';
 import { useDispatch } from 'react-redux';
 import { deleteTask } from 'services/board-servises';
+import { differenceInHours } from 'date-fns';
 
-export const EditPanelCard = ({ id }) => {
+export const EditPanelCard = ({ id, deadline }) => {
   const { isOpen, close, open } = useToggleModal();
   // const [showModal, setShowModal] = useState(false);
   const dispatch = useDispatch();
@@ -16,12 +22,20 @@ export const EditPanelCard = ({ id }) => {
     // сохранить в стейт /перерендерить страницу
     close();
   };
+
+  const is24Hours = differenceInHours(new Date(deadline), Date.now()) < 24;
+  console.log(is24Hours);
   return (
     <>
       <Wrap>
-        <svg width="16" height="16">
-          <use xlinkHref={`${icon}#${'icon-bell'}`} />
-        </svg>
+        {is24Hours && (
+          <DeadlineWrapper>
+            <DeadlineIconBlur />
+            <DeadlineIcon width="16" height="16">
+              <use xlinkHref={`${icon}#${'icon-bell'}`} />
+            </DeadlineIcon>
+          </DeadlineWrapper>
+        )}
         <SvgBtn idIcon={'icon-move'} onClick={() => {}} />
         <SvgBtn idIcon={'icon-pencil'} onClick={open} />
         <SvgBtn idIcon={'icon-trash'} onClick={onDelete} />
